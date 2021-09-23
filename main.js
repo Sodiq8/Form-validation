75f267fd5710ddf5cbd9357ae77d30b3d68c064f
Sodiq8 <68537340+Sodiq8@users.noreply.github.com>
2021-09-24 00:30:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,59 @@
-const checkRequired = document.getElementById('form');
-const checkLength = document.getElementById('username');
-const checkEmail = document.getElementById('email');
-const checkPasswordsMatch = document.getElementById('password');
-const msg = document.querySelector('.msg')
+<script>
+    function tocheck(){
+        var un = document.getElementById("username").value;
+        var em = document.getElementById("email").value;
+        var p1 = document.getElementById("password1").value;
+        var p2 = document.getElementById("password2").value;
+    
+    
+     if(p1 === "" || p1.length<6){
+      document.getElementById("p1").innerHTML = "Password must be at least 6 characters";
+        
+      document.getElementById("password1").style.borderColor = "red";
+    }
+    else {
+        
+      document.getElementById("password1").style.borderColor = "green";
+      document.getElementById("p1").innerHTML = "";
+      
+    }
+    
+   if(p1 !== p2 || p2 ===""){
+        document.getElementById("p2").innerHTML = "Passwords do not match";
+        
+      document.getElementById("password1").style.borderColor = "red";
+           document.getElementById("password2").style.borderColor = "red";
+    }
+    else{
+        
+      document.getElementById("password2").style.borderColor = "green";
+       document.getElementById("p2").innerHTML = "";
+      
+    }
 
-checkRequired.addEventListener('submit', onSubmit)
-
-function onSubmit(e) {
- e.preventDefault();
-
-if(checkLength.value === '') {
- msg.classList.add('error');
- msg.innerHTML = 'Username must be less than 15 characters';
- setTimeout(() => msg.remove(), 3000);
-} else {
-
+    if(fn === ""){
+      document.getElementById("un").innerHTML = "Please enter a valid name";
+        
+      document.getElementById("username").style.borderColor = "red";
+    }
+    else {
+        
+      document.getElementById("username").style.borderColor = "green";
+         document.getElementById("un").innerHTML = "";
+    }
+    
+     if(em === ""){
+      document.getElementById("em").innerHTML = "Please enter a valid name";
+        
+      document.getElementById("em").style.borderColor = "red";
+    }
+     else{
+      
+      document.getElementById("email").style.borderColor = "green";
+       document.getElementById("em").innerHTML = "";
+    }
 }
-
-if(checkEmail.value === '') {
- msg.innerHTML = 'Username must be less than 15 characters';
-} else {
- checkLength.setError(null);
-return true;
-}
-
-
-}
+</script>
 
 
 
